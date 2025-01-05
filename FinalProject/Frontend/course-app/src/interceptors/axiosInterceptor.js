d03b32173a91c5c96231bffbb162c8282cf36bf6
Ömer Yılmaz <omerisler20@gmail.com>
2025-01-06 01:27:39 +0300

    prod link added

--- FinalProject/Frontend/course-app/src/interceptors/axiosInterceptor.js
+++ FinalProject/Frontend/course-app/src/interceptors/axiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from '../services/authService';
 
-const API_URL = 'https://localhost:7118/api/';
+const API_URL = `${process.env.REACT_APP_API_URL}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
